24bfcf66db80619b2ad777878c7adb8f5a54b456
im-just-a-normal-user <dzapico.xbox@gmail.com>
2026-05-04 12:55:57 +0200

    Create DupeMod class for mod initialization

---
+++ src/main/java/com/example/dupemod/DupeMod.java
@@ -0,0 +1,11 @@
+package com.example.dupemod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class DupeMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        System.out.println("[DupeMod] Mod cargado correctamente");
+    }
+}
