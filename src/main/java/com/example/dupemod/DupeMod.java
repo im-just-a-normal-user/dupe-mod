fac89f17f4267dca2e24b988f213e5aa51602c9a
im-just-a-normal-user <dzapico.xbox@gmail.com>
2026-05-04 12:59:43 +0200

    Update initialization logs and load data

--- src/main/java/com/example/dupemod/DupeMod.java
+++ src/main/java/com/example/dupemod/DupeMod.java
@@ -1,11 +1,16 @@
 package com.example.dupemod;
 
+import com.example.dupemod.data.DataManager;
 import net.fabricmc.api.ModInitializer;
 
 public class DupeMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        System.out.println("[DupeMod] Mod cargado correctamente");
+        System.out.println("[DupeMod] Mod iniciando...");
+
+        DataManager.load();
+
+        System.out.println("[DupeMod] Datos cargados correctamente");
     }
 }
