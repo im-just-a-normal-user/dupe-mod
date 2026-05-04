a0b705702ead8e904911a78af13713e5f34737b3
im-just-a-normal-user <dzapico.xbox@gmail.com>
2026-05-04 12:57:08 +0200

    Create DupeData.java

---
+++ src/main/java/com/example/dupemod/data/DupeData.java
@@ -0,0 +1,8 @@
+package com.example.dupemod.data;
+
+import java.util.*;
+
+public class DupeData {
+    public Set<String> whitelist = new HashSet<>();
+    public Map<String, Integer> credits = new HashMap<>();
+}
