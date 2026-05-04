92866ed5b4f733305bdb35cea67044daaa3d7ae8
im-just-a-normal-user <dzapico.xbox@gmail.com>
2026-05-04 19:35:50 +0200

    Refactor imports and add whitelist and credits fields

--- src/main/java/com/example/dupemod/data/DupeData.java
+++ src/main/java/com/example/dupemod/data/DupeData.java
@@ -1,8 +1,15 @@
 package com.example.dupemod.data;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 
 public class DupeData {
+
     public Set<String> whitelist = new HashSet<>();
+
     public Map<UUID, Integer> credits = new HashMap<>();
+
 }
