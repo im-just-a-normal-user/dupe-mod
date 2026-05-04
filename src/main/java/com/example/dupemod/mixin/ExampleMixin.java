e4d2b666a61846f47a7b4ba9fac84a720e0cbfd3
im-just-a-normal-user <dzapico.xbox@gmail.com>
2026-05-04 20:30:14 +0200

    Update ExampleMixin to inject into runServer method
    Changed injection point from 'loadLevel' to 'runServer' for stability.

--- src/main/java/com/example/dupemod/mixin/ExampleMixin.java
+++ src/main/java/com/example/dupemod/mixin/ExampleMixin.java
@@ -8,8 +8,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadLevel")
+	@Inject(at = @At("HEAD"), method = "runServer")
 	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadLevel()V
+		// Este método "runServer" es más estable en 1.21.1
+		System.out.println("DupeMod: Mixin cargado correctamente.");
 	}
 }
