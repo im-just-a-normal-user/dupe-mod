88ee9d0adf55145a951f02339aefb6708cf6aae0
im-just-a-normal-user <dzapico.xbox@gmail.com>
2026-05-04 19:41:08 +0200

    Update DupeMod.java

--- src/main/java/com/example/dupemod/DupeMod.java
+++ src/main/java/com/example/dupemod/DupeMod.java
@@ -1,24 +1,29 @@
 package com.example.dupemod;
 
+import com.example.dupemod.command.DupeCommand;
+import com.example.dupemod.command.WhitelistCommand;
+import com.example.dupemod.data.DataManager;
 import net.fabricmc.api.ModInitializer;
-
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DupeMod implements ModInitializer {
-	public static final String MOD_ID = "dupemod";
+    public static final String MOD_ID = "dupemod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    @Override
+    public void onInitialize() {
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+        LOGGER.info("DupeMod cargado correctamente");
 
-		LOGGER.info("Hello Fabric world!");
-	}
+        // 🔥 Cargar datos JSON
+        DataManager.load();
+
+        // 🔥 Registrar comandos
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            DupeCommand.register(dispatcher);
+            WhitelistCommand.register(dispatcher);
+        });
+    }
 }
