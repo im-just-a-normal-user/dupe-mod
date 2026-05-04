7fca0f5273493768f0cbf5d40c84d356c2c6853d
im-just-a-normal-user <dzapico.xbox@gmail.com>
2026-05-04 19:36:50 +0200

    Improve file handling in DataManager
    Refactor file reading and writing to use try-with-resources for better resource management.

--- src/main/java/com/example/dupemod/data/DataManager.java
+++ src/main/java/com/example/dupemod/data/DataManager.java
@@ -10,6 +10,7 @@ import java.nio.file.Path;
 public class DataManager {
 
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
     private static final Path FILE = FabricLoader.getInstance()
             .getConfigDir()
             .resolve("dupemod.json");
@@ -23,23 +24,31 @@ public class DataManager {
                 return;
             }
 
-            Reader r = new FileReader(FILE.toFile());
-            data = GSON.fromJson(r, DupeData.class);
-            r.close();
+            try (Reader r = new FileReader(FILE.toFile())) {
+                data = GSON.fromJson(r, DupeData.class);
+            }
+
+            if (data == null) {
+                data = new DupeData();
+            }
 
             if (data.whitelist == null) data.whitelist = new java.util.HashSet<>();
             if (data.credits == null) data.credits = new java.util.HashMap<>();
 
         } catch (Exception e) {
             e.printStackTrace();
+            data = new DupeData();
         }
     }
 
     public static void save() {
         try {
-            Writer w = new FileWriter(FILE.toFile());
-            GSON.toJson(data, w);
-            w.close();
+            FILE.toFile().getParentFile().mkdirs();
+
+            try (Writer w = new FileWriter(FILE.toFile())) {
+                GSON.toJson(data, w);
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
